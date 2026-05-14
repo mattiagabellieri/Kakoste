1b2302854ccf71c7944ca717f00ff22b1edb42f3
android <android@domain.local>
2026-05-14 12:06:12 +0200

    rirpova push

--- app/src/main/java/com/example/omnitext/signup_screen_activity.kt
+++ app/src/main/java/com/example/omnitext/signup_screen_activity.kt
@@ -1,24 +1,36 @@
 package com.example.omnitext
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.ktx.firestore
-import com.google.firebase.ktx.Firebase
-import com.google.firebase.firestore.FieldValue // facoltativo per i timestamp
 
 class signup_screen_activity : AppCompatActivity() {
+
+    // 1. DICHIARA LE VARIABILI QUI (Prima del onCreate)
     private lateinit var auth: FirebaseAuth
     private lateinit var db: FirebaseFirestore
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_signup_screen)
+
+        // 2. INIZIALIZZA FIREBASE
+        auth = FirebaseAuth.getInstance()
+        db = FirebaseFirestore.getInstance()
+
+// Riferimenti agli elementi grafici (controlla che gli ID coincidano con il tuo XML)
+        val etUsername = findViewById<EditText>(R.id.etNewUsername)
+        val etPhone = findViewById<EditText>(R.id.etPhone)
+        val etPassword = findViewById<EditText>(R.id.etNewPassword)
+        val btnSignUp = findViewById<Button>(R.id.btnDoSignUp)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
